10facfce7212d5103be919b55caf0af31599f4e1
Greg Delgado <grd888@gmail.com>
2025-04-29 21:27:46 +0800

    useColorScheme for light and dark modes

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,28 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, useColorScheme, View } from "react-native";
 import { Stack } from "expo-router";
-import React from "react";
+import { Colors } from "../constants/Colors.js";
+import { StatusBar } from "expo-status-bar";
 
 const RootLayout = () => {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme] ?? Colors.light;
+
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: { backgroundColor: "#ddd" },
-        headerTintColor: "#333",
-      }}
-    >
-      <Stack.Screen
-        name="index"
-        options={{ title: "Home" }}
-      />
-      <Stack.Screen
-        name="about"
-        options={{ title: "About" }}
-      />
-      <Stack.Screen
-        name="contact"
-        options={{ title: "Contact" }}
-      />
-    </Stack>
+    <>
+      <StatusBar value="auto" />
+      <Stack
+        screenOptions={{
+          headerStyle: { backgroundColor: theme.navBackground },
+          headerTintColor: theme.title,
+        }}
+      >
+        <Stack.Screen
+          name="index"
+          options={{ title: "Home" }}
+        />
+        <Stack.Screen
+          name="about"
+          options={{ title: "About" }}
+        />
+        <Stack.Screen
+          name="contact"
+          options={{ title: "Contact" }}
+        />
+      </Stack>
+    </>
   );
 };
 
--- app/about.jsx
+++ app/about.jsx
@@ -1,18 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Link } from "expo-router"
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+import { useColorScheme } from "react-native";
+import { Colors } from "../constants/Colors.js";
 
 const About = () => {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme] ?? Colors.light;
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>About Page</Text>
+    <View style={[styles.container, { backgroundColor: theme.background }]}>
+      <Text style={[styles.title, { color: theme.text }]}>About Page</Text>
 
-      <Link href="/" style={styles.link}>Home</Link>
+      <Link
+        href="/"
+        style={styles.link}
+      >
+        Home
+      </Link>
     </View>
-  )
-}
+  );
+};
 
-export default About
+export default About;
 
 const styles = StyleSheet.create({
   container: {
@@ -26,6 +36,6 @@ const styles = StyleSheet.create({
   },
   link: {
     marginTop: 10,
-    borderBottomWidth:1
-  }
+    borderBottomWidth: 1,
+  },
 });
