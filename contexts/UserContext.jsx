f9677b1820efde0b5247398efa47173dc7aae033
Greg Delgado <grd888@gmail.com>
2025-05-05 23:14:37 +0800

    fetch the initial user

--- contexts/UserContext.jsx
+++ contexts/UserContext.jsx
@@ -1,42 +1,64 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { account } from "../lib/appwrite";
 import { ID } from "react-native-appwrite";
+import { Text } from "react-native";
+
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
+  const [authChecked, setAuthChecked] = useState(false);
 
-    async function login(email, password) {
-        try {
-            await account.createEmailPasswordSession(email, password);
-            const response = await account.get();
-            setUser(response);
-        } catch (error) {
-            throw Error(error.message); // Re-throw the error so it can be caught by the component
-        }
+  async function login(email, password) {
+    try {
+      await account.createEmailPasswordSession(email, password);
+      const response = await account.get();
+      setUser(response);
+    } catch (error) {
+      throw Error(error.message); // Re-throw the error so it can be caught by the component
     }
+  }
 
-    async function register(email, password) {
-        try {
-            await account.create(ID.unique(), email, password);
-            await login(email, password);
-        } catch (error) {
-            throw Error(error.message); // Re-throw the error so it can be caught by the component
-        }
+  async function register(email, password) {
+    try {
+      await account.create(ID.unique(), email, password);
+      await login(email, password);
+    } catch (error) {
+      throw Error(error.message); // Re-throw the error so it can be caught by the component
     }
+  }
 
-    async function logout() {
-        try {
-            await account.deleteSession("current");
-            setUser(null);
-        } catch (error) {
-            throw Error(error.message); // Re-throw the error so it can be caught by the component
-        }
+  async function logout() {
+    try {
+      await account.deleteSession("current");
+      setUser(null);
+    } catch (error) {
+      throw Error(error.message); // Re-throw the error so it can be caught by the component
     }
+  }
 
-    return (
-        <UserContext.Provider value={{ user, setUser, login, register, logout }}>
-            {children}
-        </UserContext.Provider>
-    );
+  async function getInitialUser() {
+    try {
+      const response = await account.get();
+      setUser(response);
+    } catch (error) {
+      setUser(null);
+      console.error("Error fetching initial user:", error);
+    } finally {
+      console.log("Initial user fetched");
+      setAuthChecked(true);
+    }
+  }
+
+  useEffect(() => {
+    getInitialUser();
+  }, []);
+
+  return (
+    <UserContext.Provider
+      value={{ user, login, register, logout, authChecked }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
 };
