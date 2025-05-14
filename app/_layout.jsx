b921f2b63faffe80e8428208bddb157f2137512a
Greg Delgado <grd888@gmail.com>
2025-05-15 06:36:04 +0800

    wrap the dashboard root layout with the BooksProvider

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,6 +3,7 @@ import { Stack } from "expo-router";
 import { Colors } from "../constants/Colors.js";
 import { StatusBar } from "expo-status-bar";
 import { UserProvider } from "../contexts/UserContext";
+import { BooksProvider } from "../contexts/BooksContext";
 
 const RootLayout = () => {
   const colorScheme = useColorScheme();
@@ -10,26 +11,28 @@ const RootLayout = () => {
 
   return (
     <UserProvider>
-      <StatusBar value="auto" />
-      <Stack
-        screenOptions={{
-          headerStyle: { backgroundColor: theme.navBackground },
-          headerTintColor: theme.title,
-        }}
-      >
-        <Stack.Screen
-          name="index"
-          options={{ title: "Home" }}
-        />
-        <Stack.Screen
-          name="(auth)"
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="(dashboard)"
-          options={{ headerShown: false }}
-        />
-      </Stack>
+      <BooksProvier>
+        <StatusBar value="auto" />
+        <Stack
+          screenOptions={{
+            headerStyle: { backgroundColor: theme.navBackground },
+            headerTintColor: theme.title,
+          }}
+        >
+          <Stack.Screen
+            name="index"
+            options={{ title: "Home" }}
+          />
+          <Stack.Screen
+            name="(auth)"
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="(dashboard)"
+            options={{ headerShown: false }}
+          />
+        </Stack>
+      </BooksProvier>
     </UserProvider>
   );  
 };
