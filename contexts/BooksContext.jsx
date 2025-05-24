e7b3eff8184c8e4f32f537589700aa9ed68cce44
Greg Delgado <grd888@gmail.com>
2025-05-24 17:16:19 +0800

    realtime updates

--- contexts/BooksContext.jsx
+++ contexts/BooksContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect } from "react";
-import { databases } from "../lib/appwrite";
+import { databases, client } from "../lib/appwrite";
 import { ID, Permission, Role } from "react-native-appwrite";
 import { useUser } from "../hooks/useUser";
 import { Query } from "react-native-appwrite";
@@ -13,10 +13,25 @@ export const BooksProvider = ({ children }) => {
   const { user } = useUser();
 
   useEffect(() => {
+    let unsubscribe;
+    const channel = `databases.${DATABASE_ID}.collections.${COLLECTION_ID}.documents`;
+
     if (user) { 
       fetchBooks();
+      unsubscribe = client.subscribe(channel, (response) => {
+        const { payload, events } = response;
+        if (events[0].includes('create')) {
+          setBooks((prevBooks) => [...prevBooks, payload]);
+        }
+      })
     } else {
       setBooks([]);
+    }
+
+    return () => {
+      if (unsubscribe) {
+        unsubscribe();
+      }
     }
   }, [user]);
 
